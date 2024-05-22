732ccc512fe1cddb12a699eaf45692bbb967b181
NicolasCardenas1 <n.cardenasl@duoc.cl>
2024-05-21 21:39:28 -0400

    Creacion repositorio en rama master

---
+++ funcion.js
@@ -0,0 +1,142 @@
+//funciòn que carga las animaciones de las habilidades
+function cargarAnimaciones(){
+    $(function(){
+        $('.chart1').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart2').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart3').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart4').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart5').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+        $('.chart6').easyPieChart({
+            size:160,
+            barColor:"#ff5b00",
+            scaleLength:0,
+            lineWidth:15,
+            tackColor:"#525151",
+            lineCap:"circle",
+            animate:2000,
+        });
+
+    })
+}
+
+//Función que detecta cuando comenzar la animación habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("habilidades");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("chart");
+        habilidades[0].classList.add("chart1");
+        habilidades[1].classList.add("chart2");
+        habilidades[2].classList.add("chart3");
+        habilidades[3].classList.add("chart4");
+        habilidades[4].classList.add("chart5");
+        habilidades[5].classList.add("chart6");
+
+        cargarAnimaciones();
+    }
+}
+
+//detecto el scroll para animar habilidades
+window.onscroll = function(){
+    efectoHabilidades();
+}
+
+//función que muestra el menu responsivo{
+function responsiveMenu(){
+    let x = document.getElementById("nav");
+    if(x.className===""){
+        x.className = "responsive";
+
+        //creamos el elemento que cierra el menu
+        let span = document.createElement("span");
+        span.innerHTML = "X";
+        document.getElementById("nav").appendChild(span);
+
+        //quitamos el boton eliminar cuando se hace click sobre este
+        span.onclick = function(){
+            x.className = "";
+            span.remove();
+        }
+    }else{
+        x.className="";
+    }
+}
+
+
+//ocultar menú al pinchar link
+const links = document.querySelectorAll("#nav a");
+for(i=0; i < links.length;i++){
+    links[i].onclick = function(){
+        var x = document.getElementById("nav");
+        x.className = "";
+
+        //remover botón elimionar
+        btnEliminar = document.querySelector("#nav span");
+        btnEliminar.remove();
+    }
+}
+
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var formData = new FormData(this);
+    var url = "https://formsubmit.co/cardenaslfc23@gmail.com"  
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+            "Accept": "application/json"
+        },
+        body: formData
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        alert("¡Mensaje enviado con éxito!");
+        document.getElementById("contactForm").reset();
+    })
+    .catch(function(error) {
+        console.error("Error:", error);
+        alert("Hubo un error al enviar el mensaje. Por favor, inténtalo de nuevo.");
+    });
+});
